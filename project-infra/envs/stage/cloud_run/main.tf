967aab2e34a8830f19aa5f2284a3cf7fac960800
Kyrylo OLIINYK <kyrylo.oliinyk@smile-ukraine.com>
2025-03-09 16:55:42 +0200

    new sctructure

---
+++ project-infra/envs/stage/cloud_run/main.tf
@@ -0,0 +1,19 @@
+module "cloud_run_main" {
+  source = "../../../modules/cloud_run"
+  name = var.cloud_run_main_name
+  image = var.main_image
+  region = var.region
+  env_vars = var.env_vars
+  service_account = var.service_account
+  project_id = var.project_id
+}
+
+module "cloud_run_action" {
+  source = "../../../modules/cloud_run"
+  name = var.cloud_run_actions_name
+  image = var.actions_image
+  region = var.region
+  env_vars = var.env_vars
+  service_account = var.service_account
+  project_id = var.project_id
+}
